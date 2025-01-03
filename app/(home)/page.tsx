d9c85dc9001e0308ac7d651d47d6e9d8f6032a79
Pratik Pund <93414233+pratik-git10@users.noreply.github.com>
2025-01-03 23:13:01 +0530

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,22 +4,10 @@ export default function Home() {
       <div className="max-w-3xl mx-4 md:mx-auto">
         <div className="mt-[88px] rounded-md bg--300 grid grid-cols-2 justify-center items-center h-auto p-2 mb-28">
           <div className="mx-2 border h-auto p-2 bg--400 rounded-md">
-            Hey, I am Pratik Sanjayrao Pund. Hey, I am Pratik Sanjayrao Pund.
-            Hey, I am Pratik Sanjayrao Pund. Hey, I am Pratik Sanjayrao Pund.
-            Hey, I am Pratik Sanjayrao Pund. Hey, I am Pratik Sanjayrao Pund.
-            Hey, I am Pratik Sanjayrao Pund. Hey, I am Pratik Sanjayrao Pund.
-            Hey, I am Pratik Sanjayrao Pund. Hey, I am Pratik Sanjayrao Pund.
-            Hey, I am Pratik Sanjayrao Pund. Hey, I am Pratik Sanjayrao Pund.
+            Hey, I am Pratik Sanjayrao Pund.
           </div>
           <div className="mx-2 border h-auto p-2 bg--400 rounded-md">
-            Skillset Skillset Skillset Skillset Skillset Skillset Skillset
-            Skillset Skillset Skillset Skillset Skillset Skillset Skillset
-            Skillset Skillset Skillset Skillset Skillset Skillset Skillset
-            Skillset Skillset Skillset Skillset Skillset Skillset Skillset
-            Skillset Skillset Skillset Skillset Skillset Skillset Skillset
-            Skillset Skillset Skillset Skillset Skillset Skillset Skillset
-            Skillset Skillset Skillset Skillset Skillset Skillset Skillset
-            Skillset Skillset Skillset Skillset Skillset Skillset Skillset
+            
           </div>
         </div>
       </div>
