5267190dac2631ca17ba3928952cf61daf2e47ae
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-06-11 14:48:09 +0530

    changes

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,9 @@
+import LoaderComponent from "@/components/loader";
+
 const Loader = () => {
   return (
     <div className="flex justify-end items-center my-4 mx-4">
-      <div
-        className={`animate-spin rounded-full size-7 border-2 border-t-0 border-amber-700`}
-      />
+      <LoaderComponent />
     </div>
   );
 };
--- components/loader.tsx
+++ components/loader.tsx
@@ -1,4 +1,4 @@
-const Loader = () => {
+const LoaderComponent = () => {
   return (
     <div
       className={`animate-spin rounded-full size-7 border-2 border-t-0 border-amber-700`}
@@ -6,4 +6,4 @@ const Loader = () => {
   );
 };
 
-export default Loader;
+export default LoaderComponent;
